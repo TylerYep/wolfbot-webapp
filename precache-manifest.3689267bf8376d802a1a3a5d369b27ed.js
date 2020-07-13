8d5d5788125753db146146aafd4d2aa695203508
Tyler Yep <tyep@stanford.edu>
2020-07-12 17:04:17 -0700

    Updates

---
+++ precache-manifest.3689267bf8376d802a1a3a5d369b27ed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b14c9fd52fa006555ef7d7de4225064e",
+    "url": "/wolfapp/static/media/drunk.b14c9fd5.jpg"
+  },
+  {
+    "revision": "93c8ebecae06f5cab7cb",
+    "url": "/wolfapp/static/css/main.845a84eb.chunk.css"
+  },
+  {
+    "revision": "c48052dbd49b5001f3fc",
+    "url": "/wolfapp/static/js/1.c48052db.chunk.js"
+  },
+  {
+    "revision": "8bcaeb821a60a8a6d13f",
+    "url": "/wolfapp/static/js/runtime~main.8bcaeb82.js"
+  },
+  {
+    "revision": "f3ae79a637c3a8e1a866772e32c21e57",
+    "url": "/wolfapp/static/media/seer.f3ae79a6.png"
+  },
+  {
+    "revision": "dbc7936531fb17754a5e9396770900d5",
+    "url": "/wolfapp/static/media/villager.dbc79365.png"
+  },
+  {
+    "revision": "c6c260a769461842a43b8113c5cd8a5e",
+    "url": "/wolfapp/static/media/wolf.c6c260a7.jpg"
+  },
+  {
+    "revision": "081ca704822489e653254941dddb2763",
+    "url": "/wolfapp/static/media/robber.081ca704.jpg"
+  },
+  {
+    "revision": "93c8ebecae06f5cab7cb",
+    "url": "/wolfapp/static/js/main.93c8ebec.chunk.js"
+  },
+  {
+    "revision": "0c1d356dc87c821bcf17075d6f8a5a3b",
+    "url": "/wolfapp/static/media/hunter.0c1d356d.png"
+  },
+  {
+    "revision": "fd53ee11c511a1a65d7070cd4d70ef35",
+    "url": "/wolfapp/static/media/troublemaker.fd53ee11.jpg"
+  },
+  {
+    "revision": "7ae3545f7c8a9a492a076382c6683ca8",
+    "url": "/wolfapp/static/media/mason.7ae3545f.jpg"
+  },
+  {
+    "revision": "890f933264bead728678509ea9b6235f",
+    "url": "/wolfapp/static/media/tanner.890f9332.png"
+  },
+  {
+    "revision": "dbda1a29042ef687d5c1cf9d01bb993a",
+    "url": "/wolfapp/static/media/minion.dbda1a29.jpg"
+  },
+  {
+    "revision": "29f4f6390f717a3e786b38863f4449ac",
+    "url": "/wolfapp/static/media/insomniac.29f4f639.png"
+  },
+  {
+    "revision": "b942230e04ad7a7e093a3f91e5d14f21",
+    "url": "/wolfapp/index.html"
+  }
+];
